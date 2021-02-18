7b2b2cb92647df5f8722b9380df27635b4841b15
evsik <evsevsikov@gmail.com>
2021-02-18 14:32:27 +0300

    added b-contactsSlider__connections

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,16 @@
           <option value="Санкт-Петербург">Санкт-Петербург</option>
           <option value="Москва">Москва</option>
         </select>
-
+        <div class="b-contactsSlider__tel">
+          <p>
+            <a href="tel:+78126135838">+7 (812) 613-58-38</a>
+          </p>
+        </div>
+        <div class="b-contactsSlider__workTime">
+          <p>
+            Пн–пт 10:00–19:00, сб-вс 10:00–17:00
+          </p>
+        </div>
       </div>
       <div class="b-contactsSlider__links">
 
@@ -24,18 +33,52 @@ export default {
 
 <style lang="scss">
 
+$varColorBlack: #000000;
+
 .b-header {
   padding: 0 120px;
   display: flex;
   justify-content: space-between;
-
-  .b-contactsSlider__cities {
-    border: none;
-    font-family: Open Sans,serif;
-    font-weight: 600;
-    font-size: 13px;
-    line-height: 18px;
-  }
 }
+
+.b-contactsSlider {
+
+  .b-contactsSlider__connections {
+    display: flex;
+
+    .b-contactsSlider__cities {
+      border: none;
+      font-family: Open Sans, serif;
+      font-weight: 600;
+      font-size: 13px;
+      line-height: 18px;
+      color: $varColorBlack;
+      margin-right: 20px;
+    }
+
+    .b-contactsSlider__tel {
+      //margin-right: 20px;
+
+      p {
+
+        a {
+
+          @extend .b-contactsSlider__cities;
+          text-decoration: none;
+        }
+      }
+    }
+
+    .b-contactsSlider__workTime {
+
+      p {
+        @extend .b-contactsSlider__cities;
+        margin-right: 0;
+      }
+    }
+  }
+
+}
+
 
 </style>
