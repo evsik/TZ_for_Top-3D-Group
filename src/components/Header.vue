d5d330ab58ab8a34d33b0f3e0f04402ccc8ebaf6
evsik <evsevsikov@gmail.com>
2021-02-18 15:44:12 +0300

    added b-contactsSlider__links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,12 @@
   <header class="b-header">
     <div class="b-contactsSlider">
       <div class="b-contactsSlider__connections">
-        <select name="cities" class="b-contactsSlider__cities">
-          <option value="Санкт-Петербург">Санкт-Петербург</option>
-          <option value="Москва">Москва</option>
-        </select>
+        <div class="b-contactsSlider__cities">
+          <select name="cities">
+            <option value="Санкт-Петербург">Санкт-Петербург</option>
+            <option value="Москва">Москва</option>
+          </select>
+        </div>
         <div class="b-contactsSlider__tel">
           <p>
             <a href="tel:+78126135838">+7 (812) 613-58-38</a>
@@ -18,7 +20,39 @@
         </div>
       </div>
       <div class="b-contactsSlider__links">
-
+        <div class="b-contactsSlider__writeUs">
+          <p>
+            <a href="http://top3dshop.ru">
+              Напишите нам
+            </a>
+          </p>
+        </div>
+        <div class="b-contactsSlider__mailToDir">
+          <p>
+            <a href="http://top3dshop.ru">
+              Свяжитесь с директором
+            </a>
+          </p>
+        </div>
+        <div class="b-contactsSlider__socials">
+          <ul>
+            <li class="b-contactsSlider__socialsList">
+              <a href="" class="b-contactsSlider__socialsLinks">
+                <i class="fab fa-telegram-plane"></i>
+              </a>
+            </li>
+            <li class="b-contactsSlider__socialsList">
+              <a href="" class="b-contactsSlider__socialsLinks">
+                <i class="fab fa-skype"></i>
+              </a>
+            </li>
+            <li class="b-contactsSlider__socialsList">
+              <a href="" class="b-contactsSlider__socialsLinks">
+                <i class="fas fa-phone-alt"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </header>
@@ -32,16 +66,16 @@ export default {
 </script>
 
 <style lang="scss">
-
 $varColorBlack: #000000;
 
 .b-header {
-  padding: 0 120px;
-  display: flex;
-  justify-content: space-between;
 }
 
 .b-contactsSlider {
+  display: flex;
+  justify-content: space-between;
+  background-color: #FED600;
+  padding: 9px 120px;
 
   .b-contactsSlider__connections {
     display: flex;
@@ -54,6 +88,15 @@ $varColorBlack: #000000;
       line-height: 18px;
       color: $varColorBlack;
       margin-right: 20px;
+
+      select {
+        @extend .b-contactsSlider__cities;
+        margin-top: -2px;
+        background-color: #FED600;
+        margin-right: 0;
+
+      }
+
     }
 
     .b-contactsSlider__tel {
@@ -78,6 +121,61 @@ $varColorBlack: #000000;
     }
   }
 
+  .b-contactsSlider__links {
+    display: flex;
+    justify-content: space-between;
+
+    .b-contactsSlider__writeUs {
+      font-family: Open Sans, serif;
+      font-weight: 600;
+      font-size: 13px;
+      line-height: 18px;
+      color: $varColorBlack;
+
+      p {
+        a {
+          color: $varColorBlack;
+          text-decoration: none;
+        }
+      }
+    }
+
+    .b-contactsSlider__mailToDir {
+      @extend .b-contactsSlider__writeUs;
+      margin-left: 35px;
+
+      p {
+        a {
+          color: $varColorBlack;
+          text-decoration: none;
+        }
+      }
+    }
+
+    .b-contactsSlider__socials {
+      margin-left: 35px;
+      align-self: flex-end;
+
+
+      ul {
+        display: flex;
+
+        .b-contactsSlider__socialsList {
+          list-style-type: none;
+          margin-left: 20px;
+
+          &:first-of-type {
+            margin-left: 0;
+          }
+
+          .b-contactsSlider__socialsLinks {
+            color: $varColorBlack;
+          }
+        }
+      }
+    }
+
+  }
 }
 
 
